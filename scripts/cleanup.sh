77ff73ed9ee838f5b004c1cc6fa26cebeb835fba
Sam Doran <sdoran@redhat.com>
2016-11-18 13:14:08 -0500

    Update scripts
    Remove sudo since the script is executed with sudo.
    Update cache before installing packages to ensure bzip2 package is found.
    Enable VBox Guest Additions service.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-set -e
-set -x
-
 # Fix slow DNS
-sudo sed -i 's/UseDNS.*$/UseDNS no/' /etc/ssh/sshd_config
+sed -i 's/UseDNS.*$/UseDNS no/' /etc/ssh/sshd_config
 
 # Disable TTYs in run levers 2-6
-sudo sed -i -e 's,^.*:/sbin/getty\s\+.*\s\+tty[2-6],#\0,' /etc/inittab
+sed -i -e 's,^.*:/sbin/getty\s\+.*\s\+tty[2-6],#\0,' /etc/inittab
 
 # Clean apt cache
-sudo aptitude -y clean
+aptitude -y clean
 
 # Zero out the rest of the free space using dd, then delete the written file.
 echo "Reclaming free space on disk"
-sudo dd if=/dev/zero of=/EMPTY bs=1M || :
-sudo rm -f /EMPTY
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
 
 # Add `sync` so Packer doesn't quit too early, before the large file is deleted.
-sudo sync
+sync
 
--- scripts/vbox_guest_additions.sh
+++ scripts/vbox_guest_additions.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-set -e
-set -x
+# Install packages needed for Guest Additions installation
+apt-get update
+aptitude -y install build-essential
+aptitude -y install bzip2
+aptitude -y install dkms
+aptitude -y install linux-vyatta-kbuild
 
-sudo aptitude -y install build-essential
-sudo aptitude -y install bzip2
-sudo aptitude -y install dkms
-sudo aptitude -y install linux-vyatta-kbuild
+ln -s /usr/src/linux-image/debian/build/build-amd64-none-amd64-vyos "/lib/modules/$(uname -r)/build"
 
-sudo ln -s /usr/src/linux-image/debian/build/build-amd64-none-amd64-vyos "/lib/modules/$(uname -r)/build"
-
-sudo mount -o loop,ro ~/VBoxGuestAdditions.iso /mnt/
-yes | sudo /mnt/VBoxLinuxAdditions.run || :
-sudo umount /mnt/
+# Install Guest Additions
+mount -o loop,ro ~/VBoxGuestAdditions.iso /mnt/
+/mnt/VBoxLinuxAdditions.run
+umount /mnt/
 rm -f ~/VBoxGuestAdditions.iso
+
+# Install init scripts and enable service
+cp /opt/VBoxGuestAdditions*/init/{vboxadd,vboxadd-service} /etc/init.d/
+update-rc.d vboxadd defaults
+update-rc.d vboxadd-service defaults
